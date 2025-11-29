26519687b736ecb26b6432a83f8e9a9a5af2ee12
chiba <dulcet71@gmail.com>
2025-11-30 08:25:49 +0900

    TCPサーバーソケット途中

---
+++ socket_s.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<socket.h>
+// #include <winsock2.h>
+
+int main() {
+    // WSADATA w;
+    
+    // WSAStartup (MAKEWORD(2,0), &w);
+
+    // WSACleanup;
+
+    return 0;
+}
